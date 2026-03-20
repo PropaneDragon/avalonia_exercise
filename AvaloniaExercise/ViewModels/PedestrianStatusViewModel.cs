2cde4edc1397625d802c1fa65937aee7afda14b6
Sean Walton <13510828+PropaneDragon@users.noreply.github.com>
2026-03-20 12:55:17 +0000

    Added a simple filter which also shows the number of people in each state that can be filtered. Ignored any pedestrians that have crossed as this is only a temporary state and I don't think is worth filtering on.

--- AvaloniaExercise/ViewModels/PedestrianStatusViewModel.cs
+++ AvaloniaExercise/ViewModels/PedestrianStatusViewModel.cs
@@ -4,17 +4,43 @@ using AvaloniaExercise.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
 
 namespace AvaloniaExercise.ViewModels
 {
-    public class PedestrianStatusViewModel : ObservableObject
+    public partial class PedestrianStatusViewModel : ObservableObject
     {
         private readonly IPedestrianSensorService _pedestrianSensorService;
         private readonly DispatcherTimer _intervalTimer = new(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, IntervalTimer_Tick);
+        private readonly Lock _pedestrianLock = new();
+
+        [ObservableProperty]
+        private bool _filterWaiting = false;
+
+        [ObservableProperty]
+        private bool _filterCrossing = false;
+
+        public int WaitingPedestrians
+        {
+            get
+            {
+                lock (_pedestrianLock) { return Pedestrians.Count(pedestrian => pedestrian.PedestrianStatus == PedestrianStatus.WaitingToCross); }
+            }
+        }
+
+        public int CrossingPedestrians
+        {
+            get
+            {
+                lock (_pedestrianLock) { return Pedestrians.Count(pedestrian => pedestrian.PedestrianStatus == PedestrianStatus.Crossing); }
+            }
+        }
 
         public ObservableCollection<PedestrianViewModel> Pedestrians { get; } = [];
+        public ObservableCollection<PedestrianViewModel> FilteredPedestrians { get; } = [];
 
         public PedestrianStatusViewModel(IPedestrianSensorService pedestrianSensorService)
         {
@@ -23,6 +49,8 @@ namespace AvaloniaExercise.ViewModels
 
             Pedestrians.AddRange(_pedestrianSensorService.Pedestrians.Select(pedestrian => new PedestrianViewModel(pedestrian)));
 
+            ReapplyPedestrianFilter();
+
             _intervalTimer.Start();
         }
 
@@ -30,13 +58,52 @@ namespace AvaloniaExercise.ViewModels
         {
             if (e.Operation == PedestrianOperation.Arrived)
             {
-                Pedestrians.Add(new(e.Pedestrian));
+                lock (_pedestrianLock)
+                {
+                    Pedestrians.Add(new(e.Pedestrian));
+                }
+
+                e.Pedestrian.StatusChanged += Pedestrian_StatusChanged;
             }
             else if (e.Operation == PedestrianOperation.Left)
             {
-                Pedestrians.RemoveAll(viewModel => viewModel.AssociatedPedestrian == e.Pedestrian);
+                lock (_pedestrianLock)
+                {
+                    Pedestrians.RemoveAll(viewModel => viewModel.AssociatedPedestrian == e.Pedestrian);
+                }
+
+                e.Pedestrian.StatusChanged -= Pedestrian_StatusChanged;
             }
+
+            ReapplyPedestrianFilter();
+
+            OnPropertyChanged(nameof(WaitingPedestrians));
+            OnPropertyChanged(nameof(CrossingPedestrians));
         }
+
+        private void ReapplyPedestrianFilter()
+        {
+            FilteredPedestrians.RemoveAll(pedestrian => !Pedestrians.Contains(pedestrian) || !MatchesFilter(pedestrian));
+            FilteredPedestrians.AddRange(Pedestrians.Where(pedestrian => MatchesFilter(pedestrian) && !FilteredPedestrians.Contains(pedestrian)));
+        }
+
+        private void Pedestrian_StatusChanged(object? sender, PedestrianStatus e)
+        {
+            ReapplyPedestrianFilter();
+
+            OnPropertyChanged(nameof(WaitingPedestrians));
+            OnPropertyChanged(nameof(CrossingPedestrians));
+        }
+
+        private bool MatchesFilter(PedestrianViewModel pedestrian)
+        {
+            return  (!FilterWaiting && !FilterCrossing) ||
+                    (FilterWaiting && pedestrian.IsWaiting) ||
+                    (FilterCrossing && pedestrian.IsCrossing);
+        }
+
+        partial void OnFilterWaitingChanged(bool value) => ReapplyPedestrianFilter();
+        partial void OnFilterCrossingChanged(bool value) => ReapplyPedestrianFilter();
 
         private static void IntervalTimer_Tick(object? sender, EventArgs e) => WeakReferenceMessenger.Default.Send(new TimerTickMessage());
     }
